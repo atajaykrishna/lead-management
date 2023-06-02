783c55895307f31c6626c0ca536924c49691a099
Kishanthakur <kishanthakur818@gmail.com>
2023-06-03 00:00:36 +0530

    Added production build

---
+++ build/precache-manifest.04851eeee9804cec7795af5b7f611b17.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "b7904bec02e0a04fe277",
+    "url": "/static/css/main.9fdaad13.chunk.css"
+  },
+  {
+    "revision": "b7904bec02e0a04fe277",
+    "url": "/static/js/main.b7904bec.chunk.js"
+  },
+  {
+    "revision": "e8e3b76c5bf759905946",
+    "url": "/static/js/1.e8e3b76c.chunk.js"
+  },
+  {
+    "revision": "a39deaf795aff8efb679",
+    "url": "/static/js/2.a39deaf7.chunk.js"
+  },
+  {
+    "revision": "316baba7ca5f9e773de3",
+    "url": "/static/js/runtime~main.316baba7.js"
+  },
+  {
+    "revision": "9b6c1e6b418189dc01e44de90371b35a",
+    "url": "/static/media/logo.9b6c1e6b.png"
+  },
+  {
+    "revision": "926e94d4044a1985a7b4a319a6119ee5",
+    "url": "/index.html"
+  }
+];
